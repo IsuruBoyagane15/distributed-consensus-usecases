325754cadcf9c3d789c1a52b350deef08755c86b
isuruboyagane15 <isuruboyagane15@gmail.com>
2019-09-10 11:15:04 +0530

    Add commitConsensus method

--- src/main/java/LeaderCandidate.java
+++ src/main/java/LeaderCandidate.java
@@ -1,7 +1,5 @@
 import dcf.ConsensusApplication;
 import dcf.DistributedConsensusFramework;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.graalvm.polyglot.Value;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,51 +14,39 @@ public class LeaderCandidate extends ConsensusApplication {
     }
 
     @Override
-    public Boolean onReceiving(Value result) {
-        Boolean consensusResult = result.getMember("consensus").asBoolean();
-        Value agreedValue = result.getMember("value");
-        if (consensusResult){
-            System.out.println(agreedValue);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        else{
-            System.out.println(false);
-        }
-        return consensusResult;
+    public boolean onReceiving(Value result) {
+        return result.getMember("consensus").asBoolean();
     }
 
-    public static void electLeader(String clientId, int instanceCount, String kafkaServer){
+    @Override
+    public void commitConsensus(Value value) {
+        System.out.println(value.getMember("value"));
+    }
 
-        final LeaderCandidate leaderCandidate = new LeaderCandidate(clientId, "var clientRanks = [];" +
-                "result = {consensus:false, value:null};",
-                "if(Object.keys(clientRanks).length==" + instanceCount + "){" +
-                        "var leader = null;"+
-                        "var maxRank = 0;"+
-                        "for (var i = 0; i < clientRanks.length; i++) {"+
+    public static void electLeader(String nodeId, int instanceCount, String kafkaServerAddress){
+
+        LeaderCandidate leaderCandidate = new LeaderCandidate(nodeId, "var clientRanks = [];result = {consensus:false, value:null};",
+"if(Object.keys(clientRanks).length==" + instanceCount + "){" +
+                    "result.consensus=true;" +
+                    "var leader = null;"+
+                    "var maxRank = 0;"+
+                    "for (var i = 0; i < clientRanks.length; i++) {"+
                         "if(clientRanks[i].rank > maxRank){"+
-                        "result.consensus=true;" +
-                        "result.value = clientRanks[i].client;" +
-                        "maxRank = clientRanks[i].rank;" +
+                            "result.value = clientRanks[i].client;" +
+                            "maxRank = clientRanks[i].rank;" +
                         "}" +
-                        "}" +
-                        "}" +
-                        "result;",
-                kafkaServer, "Leader");
+                    "}" +
+                "}" +
+                "result;",
+                kafkaServerAddress, "Topic9");
 
-        final DistributedConsensusFramework dcf = new DistributedConsensusFramework(leaderCandidate.getNodeId(),
-                leaderCandidate.getRuntimeJsCode(), leaderCandidate.getEvaluationJsCode(), kafkaServer, leaderCandidate
-                .getKafkaTopic(), leaderCandidate);
-
+        DistributedConsensusFramework consensusFramework = new DistributedConsensusFramework(leaderCandidate);
+        consensusFramework.start();
         System.out.println(leaderCandidate.getNodeId());
-
         int clientRank = (int)(1 + Math.random()*100);
-        dcf.writeACommand("clientRanks.push({client:\""+ leaderCandidate.getNodeId() + "\",rank:" +
-                clientRank +"});");
         System.out.println(clientRank);
+        consensusFramework.writeACommand("clientRanks.push({client:\""+ leaderCandidate.getNodeId() + "\",rank:" +
+                clientRank +"});");
     }
 
     public static void main(String args[]){
