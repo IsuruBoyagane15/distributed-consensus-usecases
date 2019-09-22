59ea9e3453d38f627777d6d02a201cd55867a9e8
isuruboyagane15 <isuruboyagane15@gmail.com>
2019-09-22 11:10:32 +0530

    Implement Two Phase Commit

--- src/main/java/TwoPCCoordinator.java
+++ src/main/java/TwoPCCoordinator.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import dcf.ConsensusApplication;
+import dcf.DistributedConsensusFramework;
+import org.graalvm.polyglot.Value;
 
-public class TwoPCCoordinator {
+public class TwoPCCoordinator extends ConsensusApplication {
+
+    private boolean votesCollected;
+
+    public TwoPCCoordinator(String nodeId, String runtimeJsCode, String evaluationJsCode, String kafkaServerAddress, String kafkaTopic) {
+        super(nodeId, runtimeJsCode, evaluationJsCode, kafkaServerAddress, kafkaTopic);
+        this.votesCollected = false;
+    }
+
+    @Override
+    public boolean onReceiving(Value value) {
+        if (!votesCollected) {
+            DistributedConsensusFramework dcf = new DistributedConsensusFramework(this);
+            if (value.getMember("votesResult").isNull()){
+            }
+            else if (value.getMember("votesResult").asBoolean()) {
+                dcf.writeACommand("result.commitOrAbort=true;");
+                votesCollected = true;
+            } else if (!value.getMember("votesResult").asBoolean()) {
+                dcf.writeACommand("result.commitOrAbort=false;");
+                votesCollected = true;
+            }
+        }
+        return !value.getMember("commitOrAbort").isNull();
+    }
+
+    @Override
+    public void commitAgreedValue(Value value) {
+        if (value.getMember("commitOrAbort").asBoolean()){
+            System.out.println(getNodeId() + " committed");
+        }
+        else{
+            System.out.println(getNodeId() + " did not commit");
+        }
+    }
+
+    public static  void twoPhaseCommit(String nodeId, String kafkaServerAddress, String kafkaTopic, int instanceCount){
+        TwoPCCoordinator twoPCCoordinator = new TwoPCCoordinator(nodeId, "var participantResponses = [];result = {coordinatorRequested:null, votesResult:null, commitOrAbort:null};",
+                "if (participantResponses.some(response => response.node == \"" + nodeId + "\" && response.vote === true)){" +
+                                    "result.coordinatorRequested = true;" +
+                                    "if(participantResponses.length == " + instanceCount + "){" +
+                                        "if(participantResponses.every(response  => response.vote == true)){" +
+                                            "result.votesResult = true;" +
+                                        "}" +
+                                        "else{" +
+                                            "result.votesResult = false;" +
+                                        "}" +
+                                    "}" +
+                                "}" +
+                                "result", kafkaServerAddress, kafkaTopic);
+
+        DistributedConsensusFramework dcf = new DistributedConsensusFramework(twoPCCoordinator);
+        dcf.start();
+        dcf.writeACommand("participantResponses.push({node:\"" + nodeId + "\",vote:true});");
+
+    }
+    public static void main(String[] args){
+        TwoPCCoordinator.twoPhaseCommit(args[0], args[1], "test4", Integer.parseInt(args[2]) );
+    }
 }
--- src/main/java/TwoPCParticipant.java
+++ src/main/java/TwoPCParticipant.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+import dcf.ConsensusApplication;
+import dcf.DistributedConsensusFramework;
+import org.graalvm.polyglot.Value;
 
-public class TwoPCParticipant {
+import java.util.Random;
+
+public class TwoPCParticipant extends ConsensusApplication {
+
+    private boolean voted;
+
+    public TwoPCParticipant(String nodeId, String runtimeJsCode, String evaluationJsCode, String kafkaServerAddress, String kafkaTopic) {
+        super(nodeId, runtimeJsCode, evaluationJsCode, kafkaServerAddress, kafkaTopic);
+        voted = false;
+    }
+
+    @Override
+    public boolean onReceiving(Value value) {
+        if (value.getMember("coordinatorRequested").asBoolean()){
+            DistributedConsensusFramework dcf = new DistributedConsensusFramework(this);
+            //vote may be a false too
+            if (!voted){
+                Random random = new Random();
+                boolean canCommit;
+                canCommit = random.nextBoolean();
+                dcf.writeACommand("if(!participantResponses.some(response => response.node == \"" + getNodeId()  + "\")){participantResponses.push({node:\""+ getNodeId() + "\",vote:" + canCommit +"});}");
+                voted = true;
+            }
+            return !value.getMember("commitOrAbort").isNull();
+        }
+        return false;
+    }
+
+    @Override
+    public void commitAgreedValue(Value value) {
+        if (value.getMember("commitOrAbort").asBoolean()){
+            System.out.println(getNodeId() + " committed");
+        }
+        else{
+            System.out.println(getNodeId() + " did not commit");
+        }
+    }
+
+    public static  void twoPhaseCommit(String nodeId, String kafkaServerAddress, String kafkaTopic, String coordinatorId, int instanceCount){
+        TwoPCParticipant twoPCParticipant = new TwoPCParticipant(nodeId, "var participantResponses = [];result = {coordinatorRequested:null, votesResult:null, commitOrAbort:null};",
+                "if (participantResponses.some(response => response.node == \"" + coordinatorId + "\" && response.vote === true)){" +
+                                    "result.coordinatorRequested = true;" +
+                                    "if(participantResponses.length == " + instanceCount + "){" +
+                                        "if(participantResponses.every(response  => response.vote == true)){" +
+                                            "result.votesResult = true;" +
+                                        "}" +
+                                        "else{" +
+                                            "result.votesResult = false;" +
+                                        "}" +
+                                    "}" +
+                                "}" +
+                                "result", kafkaServerAddress, kafkaTopic);
+
+        DistributedConsensusFramework dcf = new DistributedConsensusFramework(twoPCParticipant);
+        dcf.start();
+
+    }
+    public static void main(String[] args){
+        TwoPCParticipant.twoPhaseCommit(args[0], args[1], "test4", args[2],   Integer.parseInt(args[3]) );
+    }
 }
+
